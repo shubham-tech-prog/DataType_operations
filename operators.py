c3e562c7c6dc364de41df132ce8a993ac4ad0171
shubham-tech-prog <gundshubham219@gmail.com>
2025-06-13 10:44:36 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,140 @@
+# String Operations
+
+# String to string
+
+str1 = "Instagram" 
+str2 = "Facebook"
+print(str1 + str2)  # InstagramFacebook
+print(str1 - str2)  #Error
+print(str1 * str2)  #Error
+print(str1 / str2)  #Error
+
+
+# String to tuple
+
+str3 = "Shubham"
+tup1 = ("Viraj", 21, True, 5.9)
+print(str3 + tup1)  #Error
+print(str3 - tup1)  #Error
+print(str3 * tup1)  #Error
+print(str3 / tup1)  #Error
+
+# String to list 
+
+str4 = " I love python"
+list1 = ["apple", "banana", "mango", "orange"]
+print(str4 + list1)  #Error
+print(str4 - list1)  #Error
+print(str4 * list1)  #Error
+print(str4 / list1)  #Error
+
+# String to Set
+
+str5 = " I love java"
+set1 = {"apple", "banana", "cherry"}
+print(str5 + set1)  #Error
+print(str5 - set1)  #Error
+print(str5 * set1)  #Error
+print(str5 / set1)  #Error 
+
+# string to dict
+
+str6 = "  I Love Biryani "
+Dict1 = {"name": "Shubham", "age": 21, "course": "BBA", "city": "Pune"}
+print(str6 + Dict1) #Error 
+print(str6 - Dict1) #Error
+print(str6 * Dict1) #Error
+print(str6 / Dict1) #error
+
+# List to List
+
+list1 = ["apple", "banana", "mango", "orange"] 
+list2 = [10, 20, 30, 40, 50]
+print(list1 + list2) #['apple', 'banana', 'mango', 'orange', 10, 20, 30, 40, 50]
+print(list1 - list2) #Error
+print(list1 * list2) #Error
+print(list1 / list2) #Error
+
+# List to tuple
+
+list3 = ["apple", "banana", "mango", "orange"]
+tup3 = ("Viraj", 21, True, 5.9)
+print(list3 + tup3) #Error
+print(list3 - tup3) #Error
+print(list3 * tup3) #Error
+print(list3 / tup3) #Error
+
+# List to Set
+
+list4 = ["apple", "banana", "mango", "orange"]
+set5 = {"name": "Shubham", "age": 21, "course": "BBA", "city": "Pune"}
+print(list4 + set5) #Error
+print(list4 - set5) #Error
+print(list4 * set5) #Error
+print(list4 / set5) #Error
+
+# List to dict
+
+list5 = ["apple", "banana", "mango", "orange"]
+Dict2 = {"name": "Shubham", "age": 21, "course": "BBA", "city": "Pune"}
+print(list5 + Dict2 ) #Error
+print(list5 - Dict2) #Error
+print(list5 * Dict2) #Error
+print(list5 / Dict2) #Error
+
+# tuple to tuple 
+
+tup6 = ("Viraj", 21,True, 5.9,"shubham")
+tup7 = ("Nilesh", 25,False,3.14,"Ganesh")
+print(tup6 + tup7)  #Error
+print(tup6 - tup7)  #Error
+print(tup6 * tup7)  #Error
+print(tup6 / tup7)  #Error
+
+# tuple to set
+
+tup8 = ("Apple","Orange","Pineapple","Papaya")
+set9 = {"Potato",78,"Tomato","Mango",3.14}
+print(tup8 + set9) #Error
+print(tup8 - set9) #Error 
+print(tup8 * set9) #Error 
+print(tup8 / set9) #Error
+
+# tuple to dict
+
+tup9 = ("Apple","Orange","Pineapple","Papaya")
+dict6 = {"name": "Shubham", "age": 21, "course": "BCA", "city": "Pune"}
+print(tup9 + dict6) #Error
+print(tup9 - dict6) #Error 
+print(tup9 * dict6) #Error 
+print(tup9 / dict6) #Error
+
+# set to set
+
+set10 = {"king" , "bishop" , "queen" , "pawn" , "rook"}
+set11 = {"int" , "float" , "double" , "string" , "tuple"}
+print(set10 + set11) #Error
+print(set10 - set11) #Error 
+print(set10 * set11) #Error 
+print(set10 / set11) #Error
+
+# set to dict
+
+set12 = {"king" , "bishop" , "queen" , "pawn" , "rook"}
+dict9 = {"name": "Shubham", "age": 21, "course": "BCA", "city": "Pune"}
+print(set12 + dict9) #Error
+print(set12 - dict9) #Error 
+print(set12 * dict9) #Error 
+print(set12 / dict9) #Error
+
+# dict to dict
+dict11 = { "physics" : "thermodynamics" , "chemistry" : "Chemical kinetics" , "mathematics" : "Trignometry" , "English" : "Grammer"}
+dict12 = { "A" : "One" , "B" : "Two" , "C" : "Three" , "D" : "Four"}
+print(dict11 + dict12) #Error
+print(dict11 - dict12) #Error 
+print(dict11 * dict12) #Error 
+print(dict11 / dict12) #Error
+
+
+
+
